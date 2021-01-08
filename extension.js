f0e6fb1845fd1b8c091e0376fa1cc4eb6f2bb3dd
David Grzyb <david@worksite.ca>
2021-01-08 15:13:34 -0500

    Add regex & HoverProvider

--- extension.js
+++ extension.js
@@ -9,22 +9,20 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	let hover = vscode.languages.registerHoverProvider('php', {
+		provideHover(document, position, token) {
+			// console.log(document, position);
+			let linkRange = document.getWordRangeAtPosition(position, `/route\((\"|')(.*?)(\"|')(,|\))/`);
+			if (!linkRange) return
+			return new vscode.Hover(`Click to go to route file ${linkRange}`); 
+		}
+	});
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "laravel-go-to-named-route" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('laravel-go-to-named-route.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Laravel Go To Named Route!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(hover, disposable);
 }
 exports.activate = activate;
 
